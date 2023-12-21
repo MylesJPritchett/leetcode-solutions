441f833cc9183b0a9a07b762683a62ce94cfb326
Myles Pritchett <70435789+MylesJPritchett@users.noreply.github.com>
2023-12-21 12:00:06 +1100

    Time: 448 ms (20.69%), Space: 29.1 MB (50.98%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        hashset = set()
+        for num in nums:
+            if num in hashset:
+                return True
+            hashset.add(num)
+        
+        return False
+            

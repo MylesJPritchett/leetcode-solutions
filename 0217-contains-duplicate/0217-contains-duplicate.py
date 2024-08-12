cba5cad8a96107645005f8f16365168fafac22e1
Myles Pritchett <70435789+MylesJPritchett@users.noreply.github.com>
2024-08-13 09:19:59 +1000

    Time: 417 ms (59.50%), Space: 32 MB (46.28%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,10 +1,18 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
+#integer array with more than 1 number that is a small to medium number, could be positive or negative
+
+#if a value appears twice then true
+
+#if all distinct then false
+
+
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
         hashset = set()
         for num in nums:
             if num in hashset:
                 return True
-            hashset.add(num)
-        
+            else:
+                hashset.add(num)
         return False
-            
